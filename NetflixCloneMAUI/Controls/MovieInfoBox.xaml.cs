0d0c29c315a69473e97df60c39df256ddb67da2c
Abhay Prince <46496449+Abhayprince@users.noreply.github.com>
2023-05-17 22:51:52 +0530

    Part - 6 Bottom movie info box dynamic code, commands, vm, events etc

--- NetflixCloneMAUI/Controls/MovieInfoBox.xaml.cs
+++ NetflixCloneMAUI/Controls/MovieInfoBox.xaml.cs
@@ -1,4 +1,5 @@
 using NetflixCloneMAUI.Models;
+using System.Windows.Input;
 
 namespace NetflixCloneMAUI.Controls;
 
@@ -7,13 +8,24 @@ public partial class MovieInfoBox : ContentView
 	public static readonly BindableProperty MediaProperty = 
 		BindableProperty.Create(nameof(Media), typeof(Media), typeof(MovieInfoBox), null);
 
+	public event EventHandler Closed;
+
 	public MovieInfoBox()
 	{
 		InitializeComponent();
-	}
+		ClosedCommand = new Command(ExecuteClosedCommand);
+
+    }
     public Media Media 
 	{ 
 		get => (Media)GetValue(MovieInfoBox.MediaProperty); 
 		set => SetValue(MovieInfoBox.MediaProperty, value); 
 	}
+
+    public ICommand ClosedCommand { get; private set; }
+    private void ExecuteClosedCommand() =>
+        Closed?.Invoke(this, EventArgs.Empty);
+
+    private void Button_Clicked(object sender, EventArgs e) =>
+        Closed?.Invoke(this, EventArgs.Empty);
 }
